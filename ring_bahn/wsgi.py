476827e266a231e44839398b58a7b9cddde486dc
illumi420 <illumi.420@icloud.com>
2021-04-01 23:39:45 +0200

    Django is working

--- ring_bahn/wsgi.py
+++ ring_bahn/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ring_bahn_app project.
+WSGI config for ring_bahn project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ring_bahn_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ring_bahn.settings')
 
 application = get_wsgi_application()
