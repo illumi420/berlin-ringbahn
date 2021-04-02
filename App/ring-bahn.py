7c01559af049c2fc114dae6a8be5cd79ed328be4
illumi420 <illumi.420@icloud.com>
2021-04-03 01:07:56 +0200

    ring-bahn is calculating minutes

--- App/ring-bahn.py
+++ App/ring-bahn.py
@@ -1,7 +1,39 @@
+print("RRRRRR  IIIII NN   NN   GGGG         BBBBB     AAA   HH   HH NN   NN ")
+print("RR   RR  III  NNN  NN  GG  GG        BB   B   AAAAA  HH   HH NNN  NN")
+print("RRRRRR   III  NN N NN GG      _____  BBBBBB  AA   AA HHHHHHH NN N NN")
+print("RR  RR   III  NN  NNN GG   GG        BB   BB AAAAAAA HH   HH NN  NNN ")
+print("RR   RR IIIII NN   NN  GGGGGG        BBBBBB  AA   AA HH   HH NN   NN ")
+print(
+    "                                                             \x1B[3mS41\x1B[23m/\x1B[3mS42\x1B[23m")
+print("")
+
+
 km_long = 37, 0
 
+"""
+Lists Descreption:
 
-station_duration = [
+*ringbahn_stations: 
+ -is an Array of 26 Objects each object contaiens 3 items:
+  ('name':str(station-name),'duration':int(minutes till nex station),'is_down':bool(station status))
+ -ringbahn_stations is called in  fromTo() as parameter: stations_list.
+ -in  duration() this list is sliced and appended to in_between_stations list.
+ *temp_list:
+ -is an Array with 2 objects tooken from ringbahn_stations.
+ -each object contaiens 4 items:
+ ('name':str(station-name),'duration':int(minutes till nex station),'is_down':bool(station status),'index':int(position))
+ -in  fromTo() these 2 objects are given by user start/end input and they get an 'index' key and int(value)
+ -in  duration() ringbahn_stations is sliced based on temp_list objects index values(end station is excluded), 
+  and appended to in_between_stations list.
+  *in_between_stations:
+  -is an Array with objects sliced from ringbahn_stations based on user start/end input.
+  -in_between_stations is called in  duration() as parameter: traveled_stations.
+  *minutes_list:
+  -is an Array of integers, that are tooken from 'duration' values of objects in in_between_stations array of objects.
+  -minutes_list is called in  duration() as parameter: traveled_minutes
+"""
+
+ringbahn_stations = [
     {'name': "Südkreuz", 'duration': 2, 'is_down': False},
     {'name': "Schöneberg", 'duration': 1, 'is_down': False},
     {'name': "Innsbrucker Platz", 'duration': 2, 'is_down': False},
@@ -29,37 +61,77 @@ station_duration = [
     {'name': "Hermannsstraße", 'duration': 4, 'is_down': False},
     {'name': "Tempelhof", 'duration': 2, 'is_down': False}
 ]
-index = 0
+
 temp_list = []
-another_list = []
+in_between_stations = []
 user_input = ""
 
+"""
+-function fromTo takes user input for start/end station and a list of stations(the list is an Array and stations are Objects).
+-first loop is counting the elements, starts from 1, index is always 1,
+ in every iteration index is added to index_counter depending on list length.
+-second loop is iterating over every object in the array.
+-checking if user inputs matches any object value in key 'name'.
+-appending new 'index' keys to the matched objects with the value (index_counter-1) to get the real object position in the list.
+- appending updated objects to a temporary list outside the function
+"""
 
-def fromTo(start, end, stations_list):
+
+def fromTo(start_input, end_input, stations_list):
+    index_counter = 0
+    start_input = input(">> Departure station: ")
+    end_input = input(">> Destination station: ")
+    for index in range(1, len(stations_list)):
+        for station in stations_list:
+            if index_counter < len(stations_list):
+                #print(index_counter, station['name'], station['duration'])
+                index_counter += index
+                if (start_input == station['name']) or (end_input == station['name']):
+                    # adding index as key:value to start_input/end_input station object
+                    station['index'] = index_counter-1
+                    # appending updated start_input/end_input object to a temporary list
+                    temp_list.append(station)
+                    # print("<< ", index_counter-1,
+                    #       station['name'], station['duration'])
+
+
+minutes_list = []
+
+"""
+-function duration takes two lists as parameters.
+-assigning the 'index' key values of temp_list objects in two diffrent variables.
+-the difference between the variables is how many stations are in between.
+-slicing ringbahn_stations based on the two variables and appending them to in_between_stations.
+-taking 'duration' key value from in_between_stations objects and appending them in minutes_list.
+-adding minutes_list elements together and returning the value
+"""
+
+
+def duration(traveled_stations, traveled_minutes):
+    start = temp_list[0]['index']
+    end = temp_list[1]['index']
+    print("<< Stations till destination:", end - start)
+    # slicing ringbahn stations list depending on the index values of the objects in temp_list
+    traveled_stations.append(ringbahn_stations[start:end])
+    # print(*traveled_stations[0])
     counter = 0
-    start = input("Departure station: ")
-    end = input("Destination station: ")
-    for i in range(1, len(stations_list)):
-        for j in stations_list:
-            if counter < len(stations_list):
-                #print(counter, j['name'], j['duration'])
-                counter += i
-                if (start == j['name']) or (end == j['name']):
-                    j['index'] = counter-1
-                    temp_list.append(j)
-                    print(counter-1, j['name'], j['duration'])
+    duration_time = 0
+    for i in range(0, len(traveled_stations[0])):
+        for stations in traveled_stations:
+            if counter < len(traveled_stations[0]):
+                counter += 1
+                duration_time = stations[i]['duration']
+                traveled_minutes.append(duration_time)
+
+    # print(traveled_minutes)
+    minutes = 0
+    for elements in traveled_minutes:
+        minutes += elements
+    return minutes
 
 
-fromTo(user_input, user_input, station_duration)
+fromTo(user_input, user_input, ringbahn_stations)
+print("<< Travel time:", duration(in_between_stations, minutes_list), "minutes")
 
-first = temp_list[0]['index']
-second = temp_list[1]['index']
-another_list.append(station_duration[first:second+1])
-new_list = []
-# print(another_list[:]['duration'])
-# for q in another_list:
-#     new_list.append(q['duration'])
-# print(new_list)
-
-# print(station_duration[24]['name'], station_duration[24]['duration'],
-#     station_duration[20]['name'], station_duration[20]['duration'])
+# print(ringbahn_stations[24]['name'], ringbahn_stations[24]['duration'],
+#     ringbahn_stations[20]['name'], ringbahn_stations[20]['duration'])
