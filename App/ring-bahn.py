2044b5c08a3f3e875d6ffe41232261f5d6521630
illumi420 <illumi.420@icloud.com>
2021-04-03 02:52:11 +0200

    minor-fixing + getting ready for s42

--- App/ring-bahn.py
+++ App/ring-bahn.py
@@ -13,27 +13,27 @@ km_long = 37, 0
 """
 Lists Descreption:
 
-*ringbahn_stations: 
+*s41_stations: 
  -is an Array of 27 Objects each object contaiens 3 items:
   ('name':str(station-name),'duration':int(minutes till nex station),'is_down':bool(station status))
- -ringbahn_stations is called in  fromTo() as parameter: stations_list.
- -in  duration() this list is sliced and appended to in_between_stations list.
- *temp_list:
- -is an Array with 2 objects tooken from ringbahn_stations.
+ -s41_stations is called in  s41() as parameter: stations_list.
+ -in  duration() this list is sliced and appended to s41_in_between_stations list.
+ *s41_temp_list:
+ -is an Array with 2 objects tooken from s41_stations.
  -each object contaiens 4 items:
  ('name':str(station-name),'duration':int(minutes till nex station),'is_down':bool(station status),'index':int(position))
- -in  fromTo() these 2 objects are given by user start/end input and they get an 'index' key and int(value)
- -in  duration() ringbahn_stations is sliced based on temp_list objects index values(end station is excluded), 
-  and appended to in_between_stations list.
-  *in_between_stations:
-  -is an Array with objects sliced from ringbahn_stations based on user start/end input.
-  -in_between_stations is called in  duration() as parameter: traveled_stations.
+ -in  s41() these 2 objects are given by user start/end input and they get an 'index' key and int(value)
+ -in  duration() s41_stations is sliced based on s41_temp_list objects index values(end station is excluded), 
+  and appended to s41_in_between_stations list.
+  *s41_in_between_stations:
+  -is an Array with objects sliced from s41_stations based on user start/end input.
+  -s41_in_between_stations is called in  duration() as parameter: traveled_stations.
   *minutes_list:
-  -is an Array of integers, that are tooken from 'duration' values of objects in in_between_stations array of objects.
+  -is an Array of integers, that are tooken from 'duration' values of objects in s41_in_between_stations array of objects.
   -minutes_list is called in  duration() as parameter: traveled_minutes
 """
 
-ringbahn_stations = [
+s41_stations = [
     {'name': "Südkreuz", 'duration': 2, 'is_down': False},
     {'name': "Schöneberg", 'duration': 1, 'is_down': False},
     {'name': "Innsbrucker Platz", 'duration': 2, 'is_down': False},
@@ -60,15 +60,23 @@ ringbahn_stations = [
     {'name': "Sonnenallee", 'duration': 2, 'is_down': False},
     {'name': "Neukölln", 'duration': 1, 'is_down': False},
     {'name': "Hermannsstraße", 'duration': 4, 'is_down': False},
-    {'name': "Tempelhof", 'duration': 2, 'is_down': False}
+    {'name': "Tempelhof", 'duration': 2, 'is_down': False},
+    {'name': "Südkreuz", 'duration': 2, 'is_down': False}
 ]
 
-temp_list = []
-in_between_stations = []
+# print("S41:", *s41_stations)
+# print()
+s42_ringbahn_stations = list(reversed(s41_stations))
+# print("S42:", *s42_ringbahn_stations)
+
+s41_temp_list = []
+s42_temp_list = []
+s41_in_between_stations = []
+s42_in_between_stations = []
 user_input = ""
 
 """
--function fromTo takes user input for start/end station and a list of stations(the list is an Array and stations are Objects).
+-function s41 takes a list of stations(the list is an Array and stations are Objects).
 -first loop is counting the elements, starts from 1, index is always 1,
  in every iteration index is added to index_counter depending on list length.
 -second loop is iterating over every object in the array.
@@ -77,11 +85,12 @@ user_input = ""
 - appending updated objects to a temporary list outside the function
 """
 
+start_input = input(">> Departure station: ")
+end_input = input(">> Destination station: ")
 
-def fromTo(start_input, end_input, stations_list):
+
+def s41(stations_list):
     index_counter = 0
-    start_input = input(">> Departure station: ")
-    end_input = input(">> Destination station: ")
     for index in range(1, len(stations_list)):
         for station in stations_list:
             if index_counter < len(stations_list):
@@ -91,7 +100,7 @@ def fromTo(start_input, end_input, stations_list):
                     # adding index as key:value to start_input/end_input station object
                     station['index'] = index_counter-1
                     # appending updated start_input/end_input object to a temporary list
-                    temp_list.append(station)
+                    s41_temp_list.append(station)
                     # print("<< ", index_counter-1,
                     #       station['name'], station['duration'])
 
@@ -100,20 +109,21 @@ minutes_list = []
 
 """
 -function duration takes two lists as parameters.
--assigning the 'index' key values of temp_list objects in two diffrent variables.
+-assigning the 'index' key values of s41_temp_list objects in two diffrent variables.
 -the difference between the variables is how many stations are in between.
--slicing ringbahn_stations based on the two variables values as slicing indexes and appending them to in_between_stations.
--taking 'duration' key value from in_between_stations objects and appending them in minutes_list.
+-slicing s41_stations based on the two variables values as slicing indexes and appending them to s41_in_between_stations.
+-taking 'duration' key value from s41_in_between_stations objects and appending them in minutes_list.
 -adding minutes_list elements together and returning the value
 """
 
 
 def duration(traveled_stations, traveled_minutes):
-    start = temp_list[0]['index']
-    end = temp_list[1]['index']
-    print("<< Stations till destination:", end - start)
-    # slicing ringbahn stations list depending on the index values of the objects in temp_list
-    traveled_stations.append(ringbahn_stations[start:end])
+    s41_start = s41_temp_list[0]['index']
+    s41_end = s41_temp_list[1]['index']
+    s41_difference = s41_end - s41_start
+    print("<< Stations till destination:", s41_difference)
+    # slicing ringbahn stations list depending on the index values of the objects in s41_temp_list
+    traveled_stations.append(s41_stations[s41_start:s41_end])
     # print(*traveled_stations[0])
     counter = 0
     duration_time = 0
@@ -131,8 +141,9 @@ def duration(traveled_stations, traveled_minutes):
     return minutes
 
 
-fromTo(user_input, user_input, ringbahn_stations)
-print("<< Travel time:", duration(in_between_stations, minutes_list), "minutes")
+s41(s41_stations)
+print("<< Travel time:", duration(
+    s41_in_between_stations, minutes_list), "minutes")
 
-# print(ringbahn_stations[24]['name'], ringbahn_stations[24]['duration'],
-#     ringbahn_stations[20]['name'], ringbahn_stations[20]['duration'])
+# print(s41_stations[24]['name'], s41_stations[24]['duration'],
+#     s41_stations[20]['name'], s41_stations[20]['duration'])
